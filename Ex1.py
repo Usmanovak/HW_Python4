aa4f1a10e793a0d241a75cfdc5c11056e7725895
Karina <usmkarin@gmail.com>
2023-07-04 21:22:00 +0700

    done

---
+++ Ex1.py
@@ -0,0 +1,16 @@
+'''
+Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит 
+число А в целую степень B с помощью рекурсии.
+
+A = 3; B = 5 -> 243 (3⁵)
+    A = 2; B = 3 -> 8 '''
+
+number = int(input('Введите число: '))
+power = int(input('Введите значения степени: '))
+
+def power_num(number, power):
+    if power == 1:
+        return number
+    return number * power_num(number, power - 1)
+
+print(power_num(number, power))
